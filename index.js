32c18584297a0e70767d58e97a7e96918f43bde7
Benni Asal <benedikt.asal@gmx.de>
2019-02-19 20:59:55 +0100

    config: add initial state

---
+++ index.js
@@ -0,0 +1,37 @@
+var currentUser = {
+      gender: '',
+      nationality: '',
+      age: '',
+      education: '',
+      companysize: '',
+      location: ''
+};
+
+var activeClass = "highlighted";
+var selectedValues = document.forms["email-form"];
+    
+for (var i = 0, max = selectedValues.length; i < max; i++) {
+    selectedValues[i].onclick = function() {
+        currentUser[this.name] = this.value;
+        clearState(this.name);
+        changeView(currentUser[this.name], this.name);
+    }
+}
+
+function changeView(currentUserProp, selectorName){
+    var addHighlight = document.querySelectorAll('div.' + selectorName);
+    for (var i = 0, max = addHighlight.length; i < max; i++) {
+        if (addHighlight[i].title === currentUserProp){
+            addHighlight[i].classList.add(activeClass);
+        }
+    }
+}
+
+function clearState(selectorName){
+    var removeHighlight = document.querySelectorAll('div.' + selectorName);
+    for (var i = 0, max = removeHighlight.length; i < max; i++) {
+        if (removeHighlight[i].classList.contains(activeClass)){
+            removeHighlight[i].classList.remove(activeClass);
+        }
+    }
+}
